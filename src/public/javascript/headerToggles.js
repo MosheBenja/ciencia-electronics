e8500857999ea4983eb34a7da2143546656910d7
MosheBenja.com <03benja04@gmail.com>
2023-03-03 17:53:50 -0500

    3

--- src/public/javascript/headerToggles.js
+++ src/public/javascript/headerToggles.js
@@ -3,15 +3,26 @@ const menuNav = document.querySelector(".menuNav");
 const menuIcon = document.querySelector(".menuIcon");
 const masc = document.querySelector(".masc");
 const iconMenu = document.getElementById("iconMenu");
+const botonList = document.querySelector(".botonList");
+const cercosNav = document.querySelector(".cercosNav");
+const b1 = document.querySelector(".b1");
+const b2 = document.querySelector(".b2");
+const b3 = document.querySelector(".b3");
 
-menuIcon.addEventListener("click", () => {
+function menuNavToggles() {
     menuNav.classList.toggle("on");
     menuIcon.classList.toggle("on");
     masc.classList.toggle("on");
-});
+}
 
-masc.addEventListener("click", () => {
-    menuNav.classList.remove("on");
-    menuIcon.classList.remove("on");
-    masc.classList.remove("on");
-}); 
+function cercosNavToggles() {
+    botonList.classList.toggle("on");
+    cercosNav.classList.toggle("on");
+}
+
+menuIcon.addEventListener("click", menuNavToggles);
+masc.addEventListener("click", menuNavToggles); 
+botonList.addEventListener("click", cercosNavToggles);
+b1.addEventListener("click", cercosNavToggles);
+b2.addEventListener("click", cercosNavToggles);
+b3.addEventListener("click", cercosNavToggles);
